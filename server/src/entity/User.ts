c8f8ea7688d299a8507b7b66c507e4429626a8f3
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-04-04 11:21:35 -0400

    (update) setup graphql type to query

--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -1,19 +1,27 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm';
+import { ObjectType, Field } from 'type-graphql';
 
+// creates table name with lowercase class name
+@ObjectType()
 @Entity()
-export class User {
-	@PrimaryGeneratedColumn()
+export class User extends BaseEntity {
+	@Field()
+	@PrimaryGeneratedColumn('uuid')
 	id!: string;
 
+	@Field((type) => String)
 	@Column()
 	firstName!: string;
 
+	@Field()
 	@Column()
 	lastName!: string;
 
+	@Field()
 	@Column()
 	googleId!: string;
 
+	@Field()
 	@Column()
 	email!: string;
 }
